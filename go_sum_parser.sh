cab07a74e7165ea03d98f869254c195dd7df4f49
Mikle KOlyada <zlogene@gentoo.org>
2021-02-11 15:32:39 +0300

    initial
    Signed-off-by: Mikle KOlyada <zlogene@gentoo.org>

---
+++ go_sum_parser.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [[ ${#} -ne 1 ]]; then
+	echo "Exactly one argument required"
+
+	exit 1
+fi
+
+if [[ ! -f /usr/bin/xclip ]]; then
+	echo "Please install xclip to copy output"
+	echo "to clipboard"
+
+	exit 1
+fi
+
+rm /tmp/go.sum || true
+
+wget -c ${1} -P /tmp > /dev/null 2>&1
+
+if [[ ! -f /tmp/go.sum ]]; then
+	echo "It seems you have downloaded wrong file!"
+	echo "Please check your URL!"
+
+	exit 1
+fi
+
+cat /tmp/go.sum \
+	| cut -d " " -f 1,2 | \
+	xargs -i echo \"{}\" |
+	xclip  -sel clip
